19a8ff2eff7965294980010819e4c4cc668dd101
VLessa <vlessa901@gmail.com>
2023-08-18 14:36:54 -0300

    branch com o código da aula do dia Fri, Aug 18, 2023  2:36:54 PM

---
+++ App.js
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button} from 'react-native';
+
+
+const App = () =>{
+  const [inputValues, setInputValues] = useState({
+    input1: '',
+
+  });
+
+  const [soma, setSoma] = useState(0);
+
+  const handleInputChange = (inputName, value) =>{
+    setInputValues({...inputValues, [inputName]: value});
+  };
+    const handleSoma = () => {
+      const { input1 } = inputValues; 
+
+      const result = parseInt(intput1);
+
+      setSoma(result);
+    };
+
+    return(
+      <View style={styles.container}>
+        <Text style={styles.label}>Primeiro Número:</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={(value) => handleInputChange('input1', value)}
+        value={inputValues.input1}
+        keyboardType='numeric'
+        />
+
+<Text style={styles.label}>Segundo Número:</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={(value) => handleInputChange('input2', value)}
+        value={inputValues.input2}
+        keyboardType='numeric'
+        />
+<Text style={styles.label}>Terceiro Número:</Text>
+        <TextInput
+        style={styles.input}
+        onChangeText={(value) => handleInputChange('input3', value)}
+        value={inputValues.input3}
+        keyboardType='numeric'
+        />
+
+        <Button title='Somar' onPress={handleSoma}/>
+
+        <Text>Resultado:{soma}</Text>
+
+        
+      </View>
+    );
+
+};
+      
+    const styles = StyleSheet.create({ 
+      container:{
+        flex: 1,
+        backgroundColor: '#E0F2F1',
+        padding: 20,
+
+      },
+    });
+
+
+    export default App;
